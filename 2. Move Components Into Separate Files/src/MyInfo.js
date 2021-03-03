f22e591c1e9dfdcdf0c9595e1974e1982f3cf98d
ValentinGeorgievHristov <77337068+ValentinGeorgievHristov@users.noreply.github.com>
2021-03-03 11:01:03 +0200

    Separate Files

---
+++ 2. Move Components Into Separate Files/src/MyInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MyInfo(){
+  return(
+    <div>
+       <h1>Bob Ziroll</h1>
+       <p>This is paragraph about me....</p>
+       <ul>
+           <li>Thailand</li>
+           <li>Japan</li>
+           <li>Nordic Countries</li>
+       </ul>
+    </div>
+  )
+}
+
+
+export default MyInfo;
---
+++ 2. Move Components Into Separate Files/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyInfo from './MyInfo';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MyInfo />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
