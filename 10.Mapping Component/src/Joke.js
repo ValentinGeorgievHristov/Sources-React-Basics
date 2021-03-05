83a2800802da33a8be6745fed48f7e2f655f0d05
ValentinGeorgievHristov <77337068+ValentinGeorgievHristov@users.noreply.github.com>
2021-03-05 18:54:34 +0200

    One Change

--- 10.Mapping Component/src/Joke.js
+++ 10.Mapping Component/src/Joke.js
@@ -2,7 +2,7 @@ function Joke(props){
     return(
         <div>
             <h3 style={{display:props.question ? "block" : "none"}}>Question: {props.question}</h3>
-            <h3 style={{color: !props.question && "#888888"}}>Answer: {props.punchLine}</h3>
+            <h3>Answer: {props.punchLine}</h3>
             <hr />
         </div>
     )
