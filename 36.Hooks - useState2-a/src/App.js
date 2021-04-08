dd2321f1723de9eb5663b26bfbe32896363ac2a0
ValentinGeorgievHristov <77337068+ValentinGeorgievHristov@users.noreply.github.com>
2021-04-08 19:25:00 +0300

    Hooks - useState2-a (different sintaxis)

--- 36.Hooks - useState2-a/src/App.js
+++ 36.Hooks - useState2-a/src/App.js
@@ -1,31 +1,26 @@
 import { useState} from 'react';
 
-const App = () => { // ПО-КЪС ЗАПИС ЗА СЛУЧАИ, В КОИТО НЕ СЕ СМЕНЯТ ВИНАГИ ЗАЕДНО СТОЙНОСТИТЕ
+const App = () => {
 
-// const [state, setState] = useState({
-//     count: 11,
-//     step:1,
-// });
-
-const [count, setCount] = useState(1)
-const [step, setStep] = useState(1)
+const [state, setState] = useState({
+    count: 11,
+    step:1,
+});
 
 const onCounterButtonClickHandler = () => {
-   // setState(oldState => ({...oldState, count: oldState.count + oldState.step}));
-   setCount(oldState => oldState + step)
+    setState(oldState => ({...oldState, count: oldState.count + oldState.step}));
 }
 
 const onStepSelectChangeHandler = (e) => {
     const stepValue = Number(e.target.value);
 
-    //setState(oldState => ({...oldState, step:stepValue })); // ---> МНОГО ПО- ЛЕСЕН И КЪС СИНТАКСИС <---
-    setStep(stepValue);
+    setState(oldState => ({...oldState, step:stepValue }));
 }
 
 return(
     <div>
         <h1>Counter</h1>
-        <div>{count}</div>
+        <div>{state.count}</div>
         <button onClick={onCounterButtonClickHandler}>Increment</button>
         <label htmlFor="step">Step</label>
         <select name="step" id="step" onChange={onStepSelectChangeHandler}>
