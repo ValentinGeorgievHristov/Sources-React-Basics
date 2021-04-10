0c0a6aaae35c92634df98522662dd77efc32eb73
ValentinGeorgievHristov <77337068+ValentinGeorgievHristov@users.noreply.github.com>
2021-04-10 18:44:28 +0300

    Update

--- 36.Hooks - useState2-a/src/App.js
+++ 36.Hooks - useState2-a/src/App.js
@@ -1,26 +1,33 @@
-import { useState} from 'react';
+import { useState, useEffect} from 'react';
 
 const App = () => {
 
-const [state, setState] = useState({
-    count: 11,
-    step:1,
-});
+const [count, setCount] = useState(1)    
+const [step, setStep] = useState(1);
+const [character,setCharacter] = useState({name: 'Nobody'});
+
+useEffect(() => {
+    fetch(`https://swapi.dev/api/people/${step}`)
+        .then(res => res.json())
+        .then(result => setCharacter(result))
+},[step]);
 
 const onCounterButtonClickHandler = () => {
-    setState(oldState => ({...oldState, count: oldState.count + oldState.step}));
-}
+    //setState(oldState => ({...oldState, count: oldState.count + oldState.step}));
+    setCount(oldState => oldState + step);
+};
 
 const onStepSelectChangeHandler = (e) => {
-    const stepValue = Number(e.target.value);
+    // const stepValue = Number(e.target.value);
 
-    setState(oldState => ({...oldState, step:stepValue }));
+    // setState(oldState => ({...oldState, step:stepValue }));
+    setStep(step.value);
 }
 
 return(
     <div>
-        <h1>Counter</h1>
-        <div>{state.count}</div>
+        <h1>{character.name}'s Counter</h1>
+        <div>{count}</div>
         <button onClick={onCounterButtonClickHandler}>Increment</button>
         <label htmlFor="step">Step</label>
         <select name="step" id="step" onChange={onStepSelectChangeHandler}>
